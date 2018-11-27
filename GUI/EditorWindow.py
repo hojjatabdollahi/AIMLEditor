a6a48c5e57689e248f0c2b5188412ec35aa0dc05
jsiewierski11 <jarid.siewierski@gmail.com>
2018-11-27 15:29:51 -0700

    fixing export of imported files

--- GUI/EditorWindow.py
+++ GUI/EditorWindow.py
@@ -169,16 +169,16 @@ class EditorWindow(QMainWindow):
     def onFileExport(self):
         fname, filter = QFileDialog.getSaveFileName(self, 'Export to file')
         self.filename = QFileInfo(fname).fileName() # parsing out the filename from the path
-        Storage.exportAIML(self.filename, self.aiml)  # save as an aiml file
+        Storage.exportAIML(fname, self.editSpace.aiml)  # save as an aiml file
 
     def onFileImport(self):
         fname, filter = QFileDialog.getOpenFileName(self, "Import File")
         print("fname: " + fname)
         # fname = QFileInfo(fname).fileName() # parsing out the filename from the path
         # self.filename = os.path.splitext(os.path.basename(fname))[0] # removing extension from filename
-        self.aiml = Storage.importAIML(fname) # import the aiml file
+        self.editSpace.aiml = Storage.importAIML(fname) # import the aiml file
         print("file import successful")
-        self.editSpace.setPlainText(str(self.aiml))
+        self.editSpace.setPlainText(str(self.editSpace.aiml))
 
     def onFileSaveAs(self):
         fname, filter = QFileDialog.getSaveFileName(self, 'Save graph to file')
