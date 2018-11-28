a39eb45324740aec65a187b1f5074d93190710d4
jsiewierski11 <jarid.siewierski@gmail.com>
2018-11-28 09:54:20 -0700

    commit changes to main and .idea folder

--- main.py
+++ main.py
@@ -17,3 +17,23 @@ if __name__ == '__main__':
         sys.exit(app.exec_())
     except:
         pass
+
+
+# from PyQt5 import QtWidgets
+# import Utils.AIMLHighlighter as HL
+#
+# app = QtWidgets.QApplication([])
+# editor = QtWidgets.QPlainTextEdit()
+# editor.setStyleSheet("""QPlainTextEdit{
+# 	font-family:'Consolas';
+# 	color: #ccc;
+# 	background-color: #2b2b2b;}""")
+# highlight = HL.AIMLHIghlighter(editor.document())
+# editor.show()
+#
+# # Load syntax.py into the editor for demo purposes
+# # infile = open('Utils/AIMLHighlighter.py', 'r')
+# # print("hi")
+# # editor.setPlainText(infile.read())
+# # print("hey")
+# app.exec_()
